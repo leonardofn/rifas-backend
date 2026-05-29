a7cee8d8018d7da76b8bbbed982badf0763d3e81
Leonardo Feitosa <leonardofeitosa3010@gmail.com>
2026-05-29 08:17:59 -0300

    feat(seguranca): adiciona headers de segurança HTTP

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express, { type Express } from 'express';
+import helmet from 'helmet';
 import 'reflect-metadata';
 
 // Inicializar o express
 const app: Express = express();
+
+// Habilitar headers de segurança HTTP
+app.use(helmet());
 
 // Habilitar o uso de JSON no corpo das requisições
 app.use(express.json());
